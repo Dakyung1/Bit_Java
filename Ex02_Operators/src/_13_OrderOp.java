5d4922c3e75eb0e3dea6517e91ecf1320e8ee1e6
Dakyung1 <38394152+Dakyung1@users.noreply.github.com>
2019-03-14 11:05:22 +0900

    first

---
+++ Ex02_Operators/src/_13_OrderOp.java
@@ -0,0 +1,23 @@
+/*연산자 우선순위
+ * 절대로 연산자 우선순위를 외워서는 안된다.
+ * 1) =(대입연산자)는 반드시 우측 모두 끝나고 좌측에 대입된다.
+ * 2) 먼저 계산하고 싶은 대상은 반드시 ()괄호로 묶는다.
+ * 
+ */
+
+
+//비교연산자와 논리연산자의 우선순위
+public class _13_OrderOp {
+	public static void main(String[] args) {
+		int num0 = 10, num1 = 20, num3 =10;
+		//if(같다면 or 같다면)
+		//만약 헷갈린다면
+		//if((num0 == num1) || (num0 == num3))
+		
+		if(num0 == num1 || num0 == num3 ) {
+			System.out.println("둘 중에 하나는 같다.");
+		}
+
+	}
+
+}
